5fce6d2a5d4fb909ef738c70361955b5a4effb01
Braden Cariaga <11431488+Syzco@users.noreply.github.com>
2022-01-16 18:18:42 -0500

    Fiexd Initialized array

--- classes/Comparison/ChangeList.js
+++ classes/Comparison/ChangeList.js
@@ -88,6 +88,7 @@ class ChangeList {
         });
     }
     updateCollection(update) {
+        this.collectionUpdates = this.collectionUpdates || [];
         this.collectionUpdates.push({
             input: update
         });
